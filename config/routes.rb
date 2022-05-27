5c80d0ad7cb9b1732ae3f35f0805df24ad52a7b5
fobadara <fobadara@gmail.com>
2022-05-27 09:07:51 +0100

    Create public_recipes and shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'foods/index'
-  root 'foods#index'
+  root 'public_recipes#index'
 
   resources :foods, only: %i[index new create destroy]
+  resources :general_shopping_lists, only: %i[index]
+  resources :public_recipes, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
